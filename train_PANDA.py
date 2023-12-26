d598ce54cf0676b55140f1c5bd5b4cd20c265e98
ricky-696 <ricky321027@gmail.com>
2023-12-26 14:10:34 +0800

    sync

--- train_PANDA.py
+++ train_PANDA.py
@@ -11,8 +11,19 @@ import trainer
 from opt import arg_parse
 from utils import get_logger
 
+from Kaggle_PANDA_1st_place_solution.src.factory import get_transform, read_yaml
+from Kaggle_PANDA_1st_place_solution.src.dataset import TrainDataset
+from Kaggle_PANDA_1st_place_solution.src.models.efficientnet import CustomEfficientNet
 
 if __name__ == '__main__':
     args = arg_parse()
+    PANDA_cfg = read_yaml(fpath='Kaggle_PANDA_1st_place_solution/src/configs/final_1.yaml')
+    transform = get_transform(conf_augmentation=PANDA_cfg.Augmentation['train'])
 
-    
+    train_set = TrainDataset(
+        conf_dataset=PANDA_cfg.Data.dataset,
+        phase='train',
+        out_ch=PANDA_cfg.Model.out_channel,
+        transform=transform,
+    )
+
